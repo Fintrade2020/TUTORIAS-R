1522e09604269462c8957904f81e675ff97bb285
Laboratorio Fintrade <60900100+Fintrade2020@users.noreply.github.com>
2020-11-29 22:48:42 -0500

    Add files via upload

---
+++ Data frame.R
@@ -0,0 +1,17 @@
+#Realizado por Daniel Esteban Castiblanco Maldonado 
+
+q<-rnorm(1000, mean = 20, sd = 5)
+View(q)
+
+Empresas3<-data.frame(Empresas,q)
+Basegeneral<-data.frame(1,2,...14)
+
+View(Empresas3)
+
+
+tarea<-rnorm(5000, mean = 20, sd=5)
+View(tarea)
+
+Empresas2<-data.frame(Empresas,q)
+Empresas2
+View(Empresas2)
